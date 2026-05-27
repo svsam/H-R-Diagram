d1e676c35ec8702406e9c1e4cfda4c7071fdd394
SvS <svsseifi@gmail.com>
2026-05-27 17:04:23 +0100

    Changes to be committed: 	new file:   Figure_2.png 	modified:   H-R graph.py

--- H-R graph.py
+++ H-R graph.py
@@ -1,20 +1,33 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('gaia_hr_data_1million.csv')
 
-plt.scatter(
-    df['surface_temperature_K'],
-    df['luminosity_L_sun'],
-    s=1,
-    alpha=0.2,
+L = df['luminosity_L_sun']
+T = df['surface_temperature_K']
+
+fig, ax = plt.subplots(figsize=(10, 7))
+
+fig.patch.set_facecolor('black')
+ax.set_facecolor('black')
+
+ax.scatter(
+    T,
+    L,
+    color='white',
+    s=0.8,
+    alpha=0.6,
+    edgecolors='none',
+    rasterized=True,
 )
 
-plt.xlabel('Temperature (K)')
-plt.gca().invert_xaxis()
+ax.set_xlabel('Surface Temperature (K)', color='white')
+ax.invert_xaxis()
+ax.set_ylabel('Luminosity ($L_\\odot$)', color='white')
+ax.set_yscale('log')
+ax.set_title('Hertzsprung-Russell Diagram', color='white')
 
-plt.ylabel('Luminosity (L_sun)')
-plt.yscale('log')
+ax.tick_params(colors='white')
 
-plt.title('H-R Diagram from Gaia Data')
 plt.show()
